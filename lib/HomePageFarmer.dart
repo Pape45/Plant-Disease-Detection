2a34eb196e6bb910aed59e647b86de03cad5dca1
AichaVictor <108080792+AichaVictor@users.noreply.github.com>
2023-07-08 10:00:12 +0000

    Add files via upload

---
+++ lib/HomePageFarmer.dart
@@ -0,0 +1,142 @@
+import 'package:animated_bottom_navigation_bar/animated_bottom_navigation_bar.dart';
+import 'package:flutter/material.dart';
+
+class HomePageFarmer extends StatefulWidget{
+  const HomePageFarmer({Key? key}) : super(key: key);
+
+  @override
+  State<HomePageFarmer> createState() => _HomePageFarmerState();
+
+}
+
+class _HomePageFarmerState extends State<HomePageFarmer> {
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    int selectedIndex = 1;
+    Size size = MediaQuery.of(context).size;
+    final List<Champ> champs = [
+    Champ(
+      name: 'Mon premier champ',
+      photoUrl: 'assets/Arbre.jpg',
+    ),
+    Champ(
+    name: 'Nouveau champ',
+    photoUrl: 'assets/plus.jpg',
+    ),
+    ];
+    return Scaffold(
+      body: SingleChildScrollView(
+       child: Column(
+         crossAxisAlignment: CrossAxisAlignment.start,
+         children: [
+           Container(
+             padding: const EdgeInsets.only(top: 20),
+             child: Row(
+               mainAxisAlignment: MainAxisAlignment.spaceAround,
+               children: [
+                 Container(
+                   padding: const EdgeInsets.symmetric(
+                     horizontal: 16.0,
+                   ),
+                   width: size.width * .9,
+                   child: Row(
+                     crossAxisAlignment: CrossAxisAlignment.center,
+                     mainAxisAlignment: MainAxisAlignment.center,
+                     children: [
+                       Icon(Icons.search, color : Colors.black54.withOpacity(.6),),
+                       const Expanded(child: TextField(
+                         showCursor: false,
+                         decoration: InputDecoration(
+                           hintText: ' Rechercher une maladie',
+                           border: InputBorder.none,
+                           focusedBorder: InputBorder.none,
+
+                         ),
+                       )),
+                     ],
+                   ),
+                   decoration: BoxDecoration(
+                     color: Colors.grey[200],
+                     borderRadius: BorderRadius.circular(20),
+                   ),
+                 ),
+               ],
+             ),
+           ),
+           Container(
+             padding: const EdgeInsets.symmetric(horizontal: 12),
+             height: 50.0,
+             width: size.width,
+             child: ListView.builder(
+               scrollDirection: Axis.horizontal,
+                 itemBuilder: (BuildContext context, int index){
+               return Padding(
+                   padding: const EdgeInsets.all(8.0),
+                child: GestureDetector(
+                  onTap: (){
+                    setState(() {
+                      selectedIndex = index;
+                    });
+                  },
+
+                ),
+               );
+             }),
+           ),
+           SizedBox(
+             height: size.height * .3,
+             child: ListView.builder(
+               scrollDirection: Axis.horizontal,
+               itemCount: 2,
+               itemBuilder: (BuildContext context, int index){
+                 return Container(
+                   width: 300,
+                   margin: const EdgeInsets.symmetric(horizontal: 10),
+                   child: Stack(
+                     children: [
+                       Positioned(
+                         left: 30,
+                         right: 30,
+                         top: 50,
+                         bottom: 50,
+                         child: Image.asset(champs[index].photoUrl),
+                       ),
+                       Positioned(
+                            bottom: 15,
+                           left: 20,
+                           child: Column(
+                             crossAxisAlignment: CrossAxisAlignment.start,
+                             children: [
+                               Text(champs[index].name, style: const TextStyle(
+                                 color: Colors.white70,
+                                 fontSize: 16,
+                               ),),
+                             ],
+                           ),),
+                     ],
+                   ),
+                   decoration: BoxDecoration(
+                     color: Colors.green[800],
+                     borderRadius: BorderRadius.circular(20),
+                   ),
+                 );
+               }
+           ),
+           ),
+         ],
+       ),
+      ),
+    );
+  }
+}
+class Champ {
+  final String name;
+  final String photoUrl;
+
+  Champ({
+    required this.name,
+    required this.photoUrl,
+  });
+}
---
+++ lib/root_page.dart
@@ -0,0 +1,130 @@
+import 'package:animated_bottom_navigation_bar/animated_bottom_navigation_bar.dart';
+import 'package:flutter/material.dart';
+import 'package:page_transition/page_transition.dart';
+import 'package:plant_disease_detection/HomePageFarmer.dart';
+import 'package:plant_disease_detection/profil.dart';
+import 'package:plant_disease_detection/ContactPhyto.dart';
+
+
+class RootPage extends StatefulWidget{
+  const RootPage({Key? key}) : super(key: key);
+
+  @override
+  State<RootPage> createState() => _RootPageState();
+
+}
+
+class _RootPageState extends State<RootPage>{
+  bool _showNotification = false;
+  int _bottomNavIndex = 0;
+  bool _isMenuOpen = false;
+
+  List<Widget> Pages = const [
+  HomePageFarmer(),
+  ContactPhyto(),
+  Profil(),
+  ];
+
+  List<IconData> iconList = [
+    Icons.home,
+    Icons.map,
+    Icons.contacts_sharp,
+    Icons.person,
+  ];
+
+  List<String> titleList = [
+    'Home',
+    'Ajouter un champ',
+    'Contacts phytos',
+    'Profile',
+  ];
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          children: [
+            SizedBox(
+              height: AppBar().preferredSize.height,
+              width: AppBar().preferredSize.height,
+              child: Image.asset('assets/logo2.jpg'),
+            ),
+            const SizedBox(width: 2),
+            const Text('AgriFarm'),
+          ],
+        ),
+        backgroundColor: Colors.white,
+        actions: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(right: 9.0),
+            child : IconButton(
+              icon: const Icon(Icons.notifications),
+              tooltip: 'Notifications',
+              onPressed: () {
+                setState(() {
+
+                });
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 16.0),
+            child: IconButton(
+              icon: Icon(_isMenuOpen ? Icons.close : Icons.menu),
+              tooltip: 'Menu',
+              onPressed: () {
+                setState(() {
+                  _isMenuOpen = !_isMenuOpen;
+                });
+              },
+            ),
+          ),
+        ],
+      ),
+      body: IndexedStack(
+        index: _bottomNavIndex,
+        children: Pages,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+
+        },
+        child: Image.asset('assets/logoPhoto.png',height: 30.0),
+        backgroundColor: Colors.lightGreen[800],
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: AnimatedBottomNavigationBar(
+        splashColor : Colors.lightGreen[800],
+        activeColor: Colors.lightGreen[800],
+        inactiveColor: Colors.black.withOpacity(.5),
+        icons: iconList,
+        activeIndex: _bottomNavIndex,
+        gapLocation: GapLocation.center,
+        notchSmoothness: NotchSmoothness.softEdge,
+        onTap: (index){
+          setState(() {
+            _bottomNavIndex = index;
+          });
+        },
+      ),
+    );
+  }
+
+  Widget _buildNotification() {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Container(
+        color: Colors.yellow,
+        width: 200,
+        height: 200,
+        child: const Center(
+          child: Text(
+            'Vous avez une nouvelle notification!',
+            style: TextStyle(fontSize: 18),
+          ),
+        ),
+      ),
+    );
+  }
+}
